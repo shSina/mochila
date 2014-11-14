50cc75960008f272a1b5d21805634a12ab6e99db
shSina <sina.sharafzadeh@gmail.com>
2014-11-15 02:23:13 +0330

    base structure , gulp live reload,angular simple ctrl

---
+++ app/scripts/controllers/baseURLCtrl.js
@@ -0,0 +1,12 @@
+'use strict';
+/**
+*  Module
+*
+* Description
+*/
+angular
+	.module('app')
+	.controller('baseURLCtrl', ['$scope','$rootScope','$state',
+	  	function($scope,$rootScope,$state){
+			console.log('baseURLCtrl');
+	}]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,57 @@
 var gulp = require('gulp')
 	,bowerFiles = require('main-bower-files')
     ,inject = require('gulp-inject')
-    ,connect = require('gulp-connect')
+    ,browserSync = require('browser-sync')
+	,reload = browserSync.reload	
     ,modRewrite = require('connect-modrewrite')
+    ,sass = require('gulp-ruby-sass')
+    ,filter = require('gulp-filter')
+    ,plumber = require('gulp-plumber')
+    ,clean = require('gulp-clean')
     ,es = require('event-stream');
-
-
 
 gulp.task('bower-inject',function () {
 	gulp.src('./app/index.html')
 		.pipe(inject(gulp.src(bowerFiles(), {read: false}), {name:'bower'}))
 		.pipe(gulp.dest('./app'));
 });
-gulp.task('html-reload', function () {
-	gulp.src('./app/*.html')
-		.pipe(connect.reload())
-		.pipe(gulp.dest('./app'));
-});
-gulp.task('livereload',function(){
-	connect.server({
-		root: 'app',
-		port: process.env.LISTEN_PORT||3000,
-		host: process.env.LISTEN_BIND_IP||'0.0.0.0',
-		livereload: {
-			port: 3579
-		},
-	    middleware: function (connect) {
-	        return [
-	            modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf|\\.jpeg$ /index.html [L]']),
-	            connect().use(
-	                '/bower_components',
-	                connect.static('./bower_components')
-	            ),
-	            connect.static('.tmp')
-	        ];
-	    }
-	});
-});
-gulp.task('watch', function () {
-  gulp.watch(['./app/*.html'], ['html-reload']);
+
+gulp.task('html', function () {
+	reload();
 });
 
-gulp.task('default', ['bower-inject','livereload','watch']);
-gulp.task('default', function() {
-	gulp.start('bower-inject');
-	gulp.start('livereload');
-	gulp.start('watch')
+gulp.task('sass',function(){
+	gulp.src('./app/styles/*.scss')
+	    .pipe(sass({
+	    	sourcemap:true,
+	    	sourcemapPath:'.'
+	    }))
+	    .pipe(plumber())
+        .pipe(gulp.dest('./.tmp/styles'))
+        .pipe(filter('**/*.css'))
+        .pipe(reload({stream:true}));
 });
+
+gulp.task('livereload',function(){
+	browserSync({
+        port: process.env.LISTEN_PORT||3000,
+        server: {
+            baseDir: "app",
+            index: "index.html",
+            middleware: [modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf|\\.jpeg|\\.css.map|\\.scss$ /index.html [L]'])],
+            routes: {
+		        "/bower_components": "./bower_components",
+		        "/styles":"./.tmp/styles"
+		    }
+        },
+        startPath: '/',
+        open:false
+    });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./app/**/*.html','./app/**/*.js'], ['html']);
+  gulp.watch('./app/styles/*.scss', ['sass']);
+});
+
+gulp.task('default', ['bower-inject','livereload','sass','watch']);
