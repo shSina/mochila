30ba9777b19e989548f11a389ea48a74ef202f43
farideh <farideh.d92@gmail.com>
2015-02-01 19:45:36 +0330

    getTodayWords

--- models/itemModel.js
+++ models/itemModel.js
@@ -33,3 +33,31 @@ exports.getAllByClassId = function(classId,next){
 		return next(err,doc);
 	});	
 }
+exports.getTodayWords = function(userId , next){
+	var nowDate = {year : new Date().getFullYear() , day : new Date().getDate()}
+	console.log(nowDate);
+	item.aggregate( 
+	 {$match:{itemType:'sPost',authorId : userId}}
+	,{$project:{body:1 ,year: { $year: "$body.startDate" }, day: { $dayOfYear: "$body.startDate" }}}
+	,{$project:{body:1 ,dy : { $subtract: [nowDate.year , "$year"] } , dd : {$subtract :[nowDate.day,"$day"]} } }
+	,{$match:{dy:0, dd:{$in:[1,-31,3]}}}
+	,{$project:{body:1}}
+	,function (err,res) {
+		console.log(res);
+	});
+}
+exports.getTodayWords(ObjectId("54ce2962237dd387126882bb"));
+
+new item({
+	authorId : ObjectId("54ce2962237dd387126882bb"),
+	authorType : 'st',
+	itemType: 'sPost',
+	classId : ObjectId("54be676bfa1023a215bd0ccd"),
+	body:{
+		word : "comer",
+		definition : "eat",
+		startDate : new Date()
+	}
+}).save(function(err,dbRes){
+		console.log(err+dbRes);
+	});
