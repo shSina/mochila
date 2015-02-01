b16db87f0a66f94f77c7fcea732218168ea23912
shSina <sina.sharafzadeh@gmail.com>
2015-02-01 11:54:38 +0330

    replace sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp')
-	,bowerFiles = require('main-bower-files')
+    ,bowerFiles = require('main-bower-files')
     ,inject = require('gulp-inject')
     ,browserSync = require('browser-sync')
-	,reload = browserSync.reload
+    ,reload = browserSync.reload
     ,modRewrite = require('connect-modrewrite')
-    ,sass = require('gulp-compass')
+    ,sourcemaps = require('gulp-sourcemaps')
+    ,sass = require('gulp-sass')
     ,filter = require('gulp-filter')
     ,clean = require('gulp-clean')
     ,es = require('event-stream');
@@ -21,15 +22,14 @@ gulp.task('html', function () {
 
 gulp.task('sass',function(){
 	gulp.src('./app/styles/**/*.scss')
-	    .pipe(sass({
-            sass:'./app/styles',
-            css:'./.tmp/styles',
-            sourcemap:true
-	    }))
-        .on('error', function (err) { console.log(err.message); })
-        .pipe(gulp.dest('./.tmp/styles'))
-        .pipe(filter('**/*.css'))
-        .pipe(reload({stream:true}));
+      .pipe(sourcemaps.init())
+      // .pipe(sass({includePaths: ['./app/styles']})) if copmass added
+      .pipe(sass())
+      .on('error', function (err) { console.log(err.message); })
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./.tmp/styles'))
+      .pipe(filter('**/*.css'))
+      .pipe(reload({stream:true}));
 });
 
 gulp.task('livereload',function(){
@@ -40,10 +40,9 @@ gulp.task('livereload',function(){
             index: "index.html",
             middleware: [modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf|\\.jpeg|\\.css.map|\\.scss$ /index.html [L]'])],
             routes: {
-		        "/bower_components": "./bower_components",
-		        "/styles":"./.tmp/styles",
-                "/app":"./app"
-		    }
+			        "/bower_components": "./bower_components",
+			        "/styles":"./.tmp/styles"
+		    		}
         },
         startPath: '/',
         open:false
