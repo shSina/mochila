2d51be17390107e4ba1acd4f0536e74abdd7e67d
shSina <sina.sharafzadeh@gmail.com>
2015-06-14 18:25:30 +0430

    refresh

--- app/scripts/controllers/streamCtrl.js
+++ app/scripts/controllers/streamCtrl.js
@@ -11,11 +11,11 @@ angular
             console.log('streamCtrl');
             $scope.items = [];
 
-            eventFact.emit('createRoom',{to:'54ece3a7058cd48622672e9c'});
-            eventFact.emit('toMessage',{message:"testing",to:'54ece3a7058cd48622672e9c'});
-            eventFact.on('toMessage',function(msg){
-                console.log(msg)
-            })
+            // eventFact.emit('createRoom',{to:'54ece3a7058cd48622672e9c'});
+            // eventFact.emit('toMessage',{message:"testing",to:'54ece3a7058cd48622672e9c'});
+            // eventFact.on('toMessage',function(msg){
+            //     console.log(msg)
+            // })
             //  eventFact.on('test',function(msg){
             //     console.log(msg)
             // })
